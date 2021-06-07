4b9c41c2960aef587f252c8a8639705bab426b19
Bastien Germond <bastien.germond@epita.fr>
2021-06-07 09:25:08 +0200

    Initial commit.

---
+++ python-server-coap/server.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import struct
+import asyncio
+
+import aiocoap.resource as resource
+import aiocoap
+
+from aiocoap.numbers.codes import Code
+
+
+class LedResource(resource.Resource):
+    """Control led status"""
+
+    def __init__(self):
+        super().__init__()
+        self.led = False
+
+    async def render_get(self, request):
+        request = request
+        response = bytes([1 if self.led else 0])
+        return aiocoap.Message(payload=response)
+
+    async def render_put(self, request):
+        print(f'PUT payload {request.payload}')
+        self.led = not self.led
+        return aiocoap.Message(code=Code.CHANGED)
+
+
+class TemperatureResource(resource.Resource):
+    """Temperature publish endpoint"""
+
+    def __init__(self):
+        super().__init__()
+        self.temperature = -1.0
+
+    async def render_get(self, request):
+        request = request
+        response = struct.pack('f', self.temperature)
+        return aiocoap.Message(payload=response)
+
+    async def render_put(self, request):
+        self.temperature = struct.unpack('f', request.payload)[0]
+        print(f"PUT temperature: {self.temperature}")
+        return aiocoap.Message(code=Code.CHANGED)
+
+
+def main():
+    root = resource.Site()
+
+    root.add_resource(['.well-known', 'core'],
+                      resource.WKCResource(root.get_resources_as_linkheader))
+    root.add_resource(['led'], LedResource())
+    root.add_resource(['temperature'], TemperatureResource())
+
+    asyncio.Task(aiocoap.Context.create_server_context(root))
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == '__main__':
+    main()
